4ecab0889045ccfa3bf3523ff0e7f18fcf74ec03
思维空间 <dnyz520@126.com>
2018-07-17 10:27:27 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+  }
 </style>
