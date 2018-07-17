43ee5e6e0543dc1c11478b23a2ffe5166f59f684
思维空间 <dnyz520@126.com>
2018-07-17 15:51:38 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,11 @@
 <template>
   <div id="app">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
+    <!--<router-view></router-view>-->
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+    export default {
+      name: 'App'
     }
-  }
-}
 </script>
-
-<style>
-  #app {
-    font-family: Helvetica, sans-serif;
-    text-align: center;
-  }
-</style>
--- src/main.js
+++ src/main.js
@@ -3,16 +3,17 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
+
+import App from './App'
 // import router from './router'
+// import store from './store'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   // router,
-  components: { App },
-  template: '<App/>'
+  // store,
+  render: h => h(App)
 })
