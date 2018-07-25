e29dd7b8f5e524d25f7a6d99856caa412dcb238b
思维空间 <dnyz520@126.com>
2018-07-25 16:09:08 +0800

    项目初始化

---
+++ src/assets/style/theme/list.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    title: '经典色',
+    name: 'careyshop',
+    preview: 'static/image/theme/careyshop/preview@2x.png'
+  },
+  {
+    title: '紫罗兰',
+    name: 'violet',
+    preview: 'static/image/theme/violet/preview@2x.png'
+  },
+  {
+    title: '小镇',
+    name: 'town',
+    backgroundImage: 'static/image/bg/town-squashed.jpg',
+    preview: 'static/image/theme/town/preview@2x.png'
+  },
+  {
+    title: '星空',
+    name: 'star',
+    backgroundImage: 'static/image/bg/star-squashed.jpg',
+    preview: 'static/image/theme/star/preview@2x.png'
+  }
+]
