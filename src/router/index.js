d4046f3af700a3a544a077c771df23f97ec9a8f0
思维空间 <dnyz520@126.com>
2018-08-13 15:15:09 +0800

    增加nprogress效果

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import util from '@/utils/util'
-
-// 路由数据
 import routes from './routes'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 Vue.use(VueRouter)
+NProgress.configure({ showSpinner: false })
 
 // 导出路由 在 main.js 里使用
 const router = new VueRouter({
@@ -18,22 +20,22 @@ const router = new VueRouter({
  * 权限验证
  */
 router.beforeEach((to, from, next) => {
+  NProgress.start()
   // 验证当前路由所有的匹配中是否需要有登陆验证的
   if (to.matched.some(r => r.meta.requiresAuth)) {
     // 这里暂时将cookie里是否存有token作为验证是否登陆的条件
-    // 请根据自身业务需要修改
     const token = util.cookies.get('token')
     if (token && token !== 'undefined') {
       next()
     } else {
       // 没有登陆的时候跳转到登陆界面
-      next({
-        name: 'login'
-      })
+      next({ name: 'login' })
+      NProgress.done()
     }
   } else {
     // 不需要身份校验 直接通过
     next()
+    NProgress.done()
   }
 })
 
@@ -45,6 +47,7 @@ router.afterEach(to => {
   app.$store.commit('pageOpenNew', { name, params, query })
   // 更改标题
   util.title(to.meta.title)
+  NProgress.done()
 })
 
 export default router
