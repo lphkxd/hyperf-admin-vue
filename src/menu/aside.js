82f3cca01e3bb92d7e6ce0d96eadd85682a9f5bb
思维空间 <dnyz520@126.com>
2018-08-15 16:54:45 +0800

    固定模拟菜单

--- src/menu/aside.js
+++ src/menu/aside.js
@@ -1,4 +1,3 @@
 // 菜单 侧边栏
 export default [
-  { path: '/index', title: '首页', icon: 'home' }
 ]
--- src/menu/header.js
+++ src/menu/header.js
@@ -1,4 +1,11 @@
 // 菜单 顶栏
 export default [
-  { path: '/index', title: '首页', icon: 'home' }
+  { path: '/index', title: '首页', icon: 'home' },
+  { path: '/goods', title: '商品', icon: 'cart-plus' },
+  { path: '/order', title: '订单', icon: 'barcode' },
+  { path: '/promotion', title: '营销', icon: 'credit-card' },
+  { path: '/member', title: '会员', icon: 'user-o' },
+  { path: '/system', title: '系统', icon: 'server' },
+  { path: '/setting', title: '设置', icon: 'gear' },
+  { path: '/cloud', title: '云端', icon: 'cloud' }
 ]
