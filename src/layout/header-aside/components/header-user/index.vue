f4bda5a82d323423b49645c30c79b6d6ae1ab6ad
思维空间 <dnyz520@126.com>
2018-09-10 17:26:36 +0800

    no message

--- src/layout/header-aside/components/header-user/index.vue
+++ src/layout/header-aside/components/header-user/index.vue
@@ -2,9 +2,20 @@
   <el-dropdown class="cs-mr">
     <span class="btn-text">您好 {{info.name}}</span>
     <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item @click.native="logOff">
-        <cs-icon name="power-off" class="cs-mr-5"/>
-        注销
+      <el-dropdown-item @click.native="$open('/')">
+        <cs-icon name="external-link-square" class="cs-mr-10"/>打开前台
+      </el-dropdown-item>
+      <el-dropdown-item divided @click.native="clearCache">
+        <cs-icon name="refresh" class="cs-mr-10"/>清空缓存
+      </el-dropdown-item>
+      <el-dropdown-item @click.native="systemOptimize">
+        <cs-icon name="magic" class="cs-mr-10"/>最优设置
+      </el-dropdown-item>
+      <el-dropdown-item divided @click.native="setPassWord">
+        <cs-icon name="keyboard-o" class="cs-mr-10"/>修改密码
+      </el-dropdown-item>
+      <el-dropdown-item divided @click.native="logOff">
+        <cs-icon name="sign-out" class="cs-mr-10"/>退出账号
       </el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
@@ -30,6 +41,16 @@ export default {
         vm: this,
         confirm: true
       })
+    },
+    /**
+     * 清空缓存
+     */
+    clearCache() {
+    },
+    /**
+     * 最优设置
+     */
+    systemOptimize() {
     }
   }
 }
