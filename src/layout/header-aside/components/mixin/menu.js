4f354070fe3297ff35050e47fa4abfacd246fffc
思维空间 <dnyz520@126.com>
2018-09-12 00:53:04 +0800

    修复测试菜单在功能搜索中无法打开

--- src/layout/header-aside/components/mixin/menu.js
+++ src/layout/header-aside/components/mixin/menu.js
@@ -3,14 +3,12 @@ import util from '@/utils/util'
 export default {
   methods: {
     handleMenuSelect(index, indexPath) {
-      if (/^cs-menu-empty-\d+$/.test(index) || index === undefined) {
+      if (!index || /^cs-menu-empty-\d+$/.test(index)) {
         this.$message.warning('临时菜单')
       } else if (/^https:\/\/|http:\/\//.test(index)) {
         util.open(index)
       } else {
-        this.$router.push({
-          path: index
-        })
+        this.$router.push({ path: index })
       }
     }
   }
