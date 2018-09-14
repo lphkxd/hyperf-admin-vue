46b1a5fbc46839c3510399ec08f81cf261e79f4b
思维空间 <dnyz520@126.com>
2018-09-14 10:27:57 +0800

    按钮权限控制完成

--- src/plugin/has/index.js
+++ src/plugin/has/index.js
@@ -1,7 +1,15 @@
+import store from '@/store'
+
 export default {
   install(Vue, options) {
     Vue.prototype.$has = function(path) {
-      return true
+      const auth = store.state.careyshop.menu.sourceData
+      if (!path || !Array.isArray(auth)) {
+        return false
+      }
+
+      const index = auth.findIndex(item => item.url === path)
+      return index !== -1
     }
   }
 }
--- src/views/system/admin/member.vue
+++ src/views/system/admin/member.vue
@@ -2,6 +2,7 @@
   <cs-container>
     <template slot="header">header</template>
     Hello Member
+    <el-button v-has="'/index/help/text'" @click.native="dialogVisible = false">测试</el-button>
     <template slot="footer">footer</template>
   </cs-container>
 </template>
