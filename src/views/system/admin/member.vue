2e2e31318938c91751ae06fcf40254db4b593414
思维空间 <dnyz520@126.com>
2018-09-17 22:33:38 +0800

    依赖升级

--- src/views/system/admin/member.vue
+++ src/views/system/admin/member.vue
@@ -2,14 +2,13 @@
   <cs-container>
     <template slot="header">header</template>
     Hello Member
-    <el-button v-has="'/index/help/text'" @click.native="dialogVisible = false">测试</el-button>
     <template slot="footer">footer</template>
   </cs-container>
 </template>
 
 <script>
 export default {
-  name: 'test'
+  name: 'member'
 }
 </script>
 
