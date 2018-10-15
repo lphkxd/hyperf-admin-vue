34107762f26192312a6565b1cb081d5bc9af340f
思维空间 <dnyz520@126.com>
2018-10-16 00:09:07 +0800

    添加API接口

--- src/api/group.js
+++ src/api/group.js
@@ -29,3 +29,90 @@ export function setAuthGroupItem(data) {
     data
   })
 }
+
+/**
+ * 获取一个用户组
+ * @param {Number} group_id
+ */
+export function getAuthGroupItem(group_id) {
+  return request({
+    url: '/v1/auth_group',
+    method: 'post',
+    params: {
+      method: 'get.auth.group.item'
+    },
+    data: {
+      group_id
+    }
+  })
+}
+
+/**
+ * 删除一个用户组
+ * @param {Number} group_id
+ */
+export function delAuthGroupItem(group_id) {
+  return request({
+    url: '/v1/auth_group',
+    method: 'post',
+    params: {
+      method: 'del.auth.group.item'
+    },
+    data: {
+      group_id
+    }
+  })
+}
+
+/**
+ * 获取用户组列表
+ * @param {Array} data
+ */
+export function getAuthGroupList(data) {
+  return request({
+    url: '/v1/auth_group',
+    method: 'post',
+    params: {
+      method: 'get.auth.group.list'
+    },
+    data
+  })
+}
+
+/**
+ * 批量设置用户组状态
+ * @param {Number} group_id
+ * @param {Number} status
+ */
+export function setAuthGroupStatus(group_id, status) {
+  return request({
+    url: '/v1/auth_group',
+    method: 'post',
+    params: {
+      method: 'set.auth.group.status'
+    },
+    data: {
+      group_id,
+      status
+    }
+  })
+}
+
+/**
+ * 设置用户组排序值
+ * @param {Number} group_id
+ * @param {Number} sort
+ */
+export function setAuthGroupSort(group_id, sort) {
+  return request({
+    url: '/v1/auth_group',
+    method: 'post',
+    params: {
+      method: 'set.auth.group.sort'
+    },
+    data: {
+      group_id,
+      sort
+    }
+  })
+}
