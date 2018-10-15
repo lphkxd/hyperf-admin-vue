6c9f78e416b8bd934927447e1e5dd0f5b28c277d
思维空间 <dnyz520@126.com>
2018-10-15 11:06:33 +0800

    新增API接口

--- src/api/admin.js
+++ src/api/admin.js
@@ -96,3 +96,103 @@ export function addAdminItem(data) {
     data
   })
 }
+
+/**
+ * 编辑一个账号
+ * @param data
+ */
+export function setAdminItem(data) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'set.admin.item'
+    },
+    data
+  })
+}
+
+/**
+ * 批量设置账号状态
+ * @param client_id array
+ * @param status
+ */
+export function setAdminStatus(client_id, status) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'set.admin.status'
+    },
+    data: {
+      client_id,
+      status
+    }
+  })
+}
+
+/**
+ * 重置一个账号密码
+ * @param client_id int
+ */
+export function resetAdminItem(client_id) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'reset.admin.item'
+    },
+    data: {
+      client_id
+    }
+  })
+}
+
+/**
+ * 批量删除账号
+ * @param client_id array
+ */
+export function delAdminList(client_id) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'del.admin.list'
+    },
+    data: {
+      client_id
+    }
+  })
+}
+
+/**
+ * 获取一个账号
+ * @param client_id int
+ */
+export function getAdminItem(client_id) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'get.admin.item'
+    },
+    data: {
+      client_id
+    }
+  })
+}
+
+/**
+ * 获取账号列表
+ * @param data
+ */
+export function getAdminList(data) {
+  return request({
+    url: '/v1/admin',
+    method: 'post',
+    params: {
+      method: 'get.admin.list'
+    },
+    data
+  })
+}
