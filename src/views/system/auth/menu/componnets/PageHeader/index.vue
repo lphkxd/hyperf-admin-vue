2841a8bc390c364c26c57d2ce0a383e623a6e567
思维空间 <dnyz520@126.com>
2018-10-31 23:42:20 +0800

    菜单模块构架中

---
+++ src/views/system/auth/menu/componnets/PageHeader/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+    <el-form-item label="菜单深度" prop="level">
+      <el-input-number
+        v-model="form.level"
+        controls-position="right"
+        :min="0"
+        label="请输入菜单深度值"
+        @keyup.enter.native="handleFormSubmit"
+        style="width: 160px;"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        :disabled="loading"
+        @click="handleFormSubmit">
+        <cs-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <cs-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      default: false
+    }
+  },
+  data() {
+    return {
+      form: {
+        level: 0
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit() {
+      this.$emit('submit', this.form)
+    },
+    handleFormReset() {
+      this.$refs.form.resetFields()
+    }
+  }
+}
+</script>
--- src/views/system/auth/menu/index.vue
+++ src/views/system/auth/menu/index.vue
@@ -1,17 +1,36 @@
 <template>
   <cs-container>
-    <template slot="header">header</template>
-    Hello Menu
-    <template slot="footer">footer</template>
+    <page-header
+      slot="header"
+      :loading="loading"
+      @submit="handleSubmit"
+      ref="header"/>
+    <page-main
+      :tree-data="tree"
+      :loading="loading"/>
   </cs-container>
 </template>
 
 <script>
 export default {
-  name: 'menus'
+  name: 'system-auth-menu',
+  components: {
+    'PageHeader': () => import('./componnets/PageHeader'),
+    'PageMain': () => import('./componnets/PageMain')
+  },
+  data() {
+    return {
+      tree: [],
+      loading: false
+    }
+  },
+  mounted() {
+    this.handleSubmit()
+  },
+  methods: {
+    // 提交查询请求
+    handleSubmit(form) {
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
