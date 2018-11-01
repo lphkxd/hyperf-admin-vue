c7faff0cb738bae6a81464c1c5894d09b7aba00f
思维空间 <dnyz520@126.com>
2018-11-02 00:38:08 +0800

    vue版本升级

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,47 +13,47 @@ module.exports = {
   rules: {
     'accessor-pairs': 2,
     'arrow-spacing': [2, {
-      'before': true,
-      'after': true
+      before: true,
+      after: true
     }],
     'block-spacing': [2, 'always'],
     'brace-style': [2, '1tbs', {
-      'allowSingleLine': true
+      allowSingleLine: true
     }],
-    'camelcase': [0, {
-      'properties': 'always'
+    camelcase: [0, {
+      properties: 'always'
     }],
     'comma-dangle': [2, 'never'],
     'comma-spacing': [2, {
-      'before': false,
-      'after': true
+      before: false,
+      after: true
     }],
     'comma-style': [2, 'last'],
     'constructor-super': 2,
-    'curly': [2, 'multi-line'],
+    curly: [2, 'multi-line'],
     'dot-location': [2, 'property'],
     'eol-last': 2,
-    'eqeqeq': [2, 'allow-null'],
+    eqeqeq: [2, 'allow-null'],
     'generator-star-spacing': [2, {
-      'before': true,
-      'after': true
+      before: true,
+      after: true
     }],
     'handle-callback-err': [2, '^(err|error)$'],
-    'indent': [2, 2, {
-      'SwitchCase': 1
+    indent: [2, 2, {
+      SwitchCase: 1
     }],
     'jsx-quotes': [2, 'prefer-single'],
     'key-spacing': [2, {
-      'beforeColon': false,
-      'afterColon': true
+      beforeColon: false,
+      afterColon: true
     }],
     'keyword-spacing': [2, {
-      'before': true,
-      'after': true
+      before: true,
+      after: true
     }],
     'new-cap': [2, {
-      'newIsCap': true,
-      'capIsNew': false
+      newIsCap: true,
+      capIsNew: false
     }],
     'new-parens': 2,
     'no-array-constructor': 2,
@@ -86,15 +86,15 @@ module.exports = {
     'no-iterator': 2,
     'no-label-var': 2,
     'no-labels': [2, {
-      'allowLoop': false,
-      'allowSwitch': false
+      allowLoop: false,
+      allowSwitch: false
     }],
     'no-lone-blocks': 2,
     'no-mixed-spaces-and-tabs': 2,
     'no-multi-spaces': 2,
     'no-multi-str': 2,
     'no-multiple-empty-lines': [2, {
-      'max': 1
+      max: 1
     }],
     'no-native-reassign': 2,
     'no-negated-in-lhs': 2,
@@ -124,13 +124,13 @@ module.exports = {
     'no-unexpected-multiline': 2,
     'no-unmodified-loop-condition': 2,
     'no-unneeded-ternary': [2, {
-      'defaultAssignment': false
+      defaultAssignment: false
     }],
     'no-unreachable': 2,
     'no-unsafe-finally': 2,
     'no-unused-vars': [2, {
-      'vars': 'all',
-      'args': 'none'
+      vars: 'all',
+      args: 'none'
     }],
     'no-useless-call': 2,
     'no-useless-computed-key': 2,
@@ -139,43 +139,43 @@ module.exports = {
     'no-whitespace-before-property': 2,
     'no-with': 2,
     'one-var': [2, {
-      'initialized': 'never'
+      initialized: 'never'
     }],
     'operator-linebreak': [2, 'after', {
-      'overrides': {
+      overrides: {
         '?': 'before',
         ':': 'before'
       }
     }],
     'padded-blocks': [2, 'never'],
-    'quotes': [2, 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
+    quotes: [2, 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: true
     }],
-    'semi': [2, 'never'],
+    semi: [2, 'never'],
     'semi-spacing': [2, {
-      'before': false,
-      'after': true
+      before: false,
+      after: true
     }],
     'space-before-blocks': [2, 'always'],
     'space-before-function-paren': [2, 'never'],
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
     'space-unary-ops': [2, {
-      'words': true,
-      'nonwords': false
+      words: true,
+      nonwords: false
     }],
     'spaced-comment': [2, 'always', {
-      'markers': ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
+      markers: ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
     }],
     'template-curly-spacing': [2, 'never'],
     'use-isnan': 2,
     'valid-typeof': 2,
     'wrap-iife': [2, 'any'],
     'yield-star-spacing': [2, 'both'],
-    'yoda': [2, 'never'],
+    yoda: [2, 'never'],
     'prefer-const': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': 0,
     'object-curly-spacing': [2, 'always', {
       objectsInObjects: false
     }],
