8d817c985edea8add48d474afaaa22a8c79db090
思维空间 <dnyz520@126.com>
2018-11-02 15:25:36 +0800

    no message

--- src/views/system/auth/menu/componnets/PageMain/index.vue
+++ src/views/system/auth/menu/componnets/PageMain/index.vue
@@ -19,17 +19,27 @@
         <el-button-group>
           <el-button
             :disabled="loading"
-            @click="() => {}">
+            @click="handleExpand">
             <cs-icon name="plus-square-o"/>
             展开
           </el-button>
           <el-button
             :disabled="loading"
-            @click="() => {}">
+            @click="handleCollapse">
             <cs-icon name="minus-square-o"/>
             收起
           </el-button>
         </el-button-group>
+      </el-form-item>
+
+      <el-form-item label="菜单过滤">
+        <el-input
+          v-model="filterText"
+          :disabled="loading"
+          placeholder="输入关键字进行过滤"
+          clearable
+          style="width: 170px;">
+        </el-input>
       </el-form-item>
 
       <el-form-item label="菜单模型">
@@ -57,6 +67,22 @@
         </el-button>
       </el-popover>
     </el-form>
+
+    <el-row :gutter="20">
+      <el-col :span="10">
+        <el-tree
+          v-loading="loading"
+          :data="currentTreeData"
+          node-key="menu_id"
+          :props="defaultProps"
+          @node-click="() => {}">
+        </el-tree>
+      </el-col>
+
+      <el-col :span="14">
+        word
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -73,7 +99,13 @@ export default {
   data() {
     return {
       radio: 'admin',
+      currentTreeData: [],
       helpContent: '暂无帮助内容',
+      filterText: '',
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
       auth: {
         add: true,
         del: true,
@@ -83,10 +115,31 @@ export default {
       }
     }
   },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    },
+    treeData: {
+      handler(val) {
+        this.currentTreeData = val
+      },
+      immediate: true
+    }
+  },
   methods: {
+    filterNode(value, data) {
+      if (!value) { return true }
+      return data.label.indexOf(value) !== -1
+    },
     // 切换模块
     handleRadio() {
       this.$emit('module', this.radio)
+    },
+    // 全部展开
+    handleExpand() {
+    },
+    // 全部收起
+    handleCollapse() {
     }
   }
 }
