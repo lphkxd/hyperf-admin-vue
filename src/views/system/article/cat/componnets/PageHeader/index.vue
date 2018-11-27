49e331a5fe097c1898b9a5dacfd26e74b5f3e6e7
思维空间 <dnyz520@126.com>
2018-11-28 00:01:21 +0800

    no message

--- src/views/system/article/cat/componnets/PageHeader/index.vue
+++ src/views/system/article/cat/componnets/PageHeader/index.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>
-  </div>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+    <el-form-item label="菜单深度" prop="level">
+      <el-input-number
+        v-model="form.level"
+        controls-position="right"
+        :min="0"
+        label="请输入菜单深度值"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        :disabled="loading"
+        @click="handleFormSubmit">
+        <cs-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <cs-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    }
+  },
+  data() {
+    return {
+      form: {
+        level: 0
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit() {
+      // 对深度进行特殊处理
+      this.$emit('submit', {
+        ...this.form,
+        level: this.form.level <= 0 ? undefined : this.form.level - 1
+      })
+    },
+    handleFormReset() {
+      this.$refs.form.resetFields()
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/views/system/article/cat/index.vue
+++ src/views/system/article/cat/index.vue
@@ -1,13 +1,42 @@
 <template>
   <cs-container>
-    <template slot="header">
-      Page 1 header
-    </template>
-    Hello World
+    <page-header
+      slot="header"
+      :loading="loading"
+      @submit="handleSubmit"
+      ref="header"/>
   </cs-container>
 </template>
 
 <script>
+import util from '@/utils/util'
+
 export default {
+  name: 'system-article-cat',
+  components: {
+    'PageHeader': () => import('./componnets/PageHeader'),
+    'PageMain': () => import('./componnets/PageMain')
+  },
+  data() {
+    return {
+      tree: [],
+      loading: false
+    }
+  },
+  mounted() {
+    this.handleSubmit()
+  },
+  methods: {
+    // 重新载入页面
+    handleRefresh() {
+      this.$nextTick(() => {
+        this.$refs.header.handleFormSubmit()
+      })
+    },
+    // 提交查询请求
+    handleSubmit(form) {
+      this.loading = true
+    }
+  }
 }
 </script>
