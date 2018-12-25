15fb2f350b4cfc8b9e38267891be670818f9c4df
思维空间 <dnyz520@126.com>
2018-12-26 00:23:05 +0800

    no message

--- src/components/cs-upload/components/cs-single.vue
+++ src/components/cs-upload/components/cs-single.vue
@@ -1,22 +1,51 @@
 <template>
   <div>
+    <el-upload
+      :action="uploadUrl"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove">
+      <i class="el-icon-plus"></i>
+    </el-upload>
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import uploadToken from './mixins/upload'
+import upload from './mixins/upload'
 
 export default {
   name: 'cs-single',
   mixins: [
-    uploadToken
+    upload
   ],
   data() {
     return {
+      dialogImageUrl: '',
+      dialogVisible: false
+    }
+  },
+  methods: {
+    // 删除资源
+    handleRemove(file, fileList) {
+      const response = file.response.data
+      if (response) {
+        this.removeStorage(response.storage_id)
+      }
+    },
+    // 资源预览
+    handlePictureCardPreview(file) {
+      const response = file.response.data
+      if (response && response.type === 0) {
+        this.dialogVisible = true
+        this.dialogImageUrl = file.url
+        return
+      }
+
+      this.$message.error('上传未完成或资源不可预览')
     }
   }
 }
 </script>
-
-<style scoped>
-</style>
--- src/components/cs-upload/components/mixins/upload.js
+++ src/components/cs-upload/components/mixins/upload.js
@@ -1,17 +1,24 @@
 import { getUploadToken } from '@/api/upload/upload'
+import { delStorageList } from '@/api/upload/storage'
 
 export default {
   data() {
     return {
-      token: {}
+      token: {},
+      uploadUrl: ''
     }
   },
   mounted() {
     getUploadToken()
       .then(res => {
         this.token = res.data
+        this.uploadUrl = this.token.token.upload_url.upload_url
       })
   },
   methods: {
+    // 批量删除资源
+    removeStorage(storage_id) {
+      delStorageList(Array.isArray(storage_id) ? storage_id : [storage_id])
+    }
   }
 }
