597fd55a1c4a24e2dbe9b8eadfc321845c97e807
思维空间 <dnyz520@126.com>
2019-01-09 17:08:47 +0800

    富文本,配置中.

--- src/components/cs-tinymce/config/config.js
+++ src/components/cs-tinymce/config/config.js
@@ -1,12 +1,9 @@
 // Any plugins you want to use has to be imported
 // Detail plugins list see https://www.tinymce.com/docs/plugins/
 // Custom builds see https://www.tinymce.com/download/custom-builds/
-const plugins = []
+export const plugins = 'advlist anchor autolink autosave charmap code codesample colorpicker contextmenu directionality emoticons fullscreen hr image imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print searchreplace tabfocus table textcolor textpattern visualblocks visualchars wordcount'
 
 // Here is a list of the toolbar
 // Detail list see https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
-const toolbar = []
-
-const config = {}
-
-export default { config, plugins, toolbar }
+// 中文注释 https://blog.csdn.net/zjiang1994/article/details/79880806
+export const toolbar = 'code | undo redo | formatselect | fontselect | fontsizeselect'
--- src/components/cs-tinymce/index.vue
+++ src/components/cs-tinymce/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <label>
-      <textarea class="tinymce-textarea" :id="tinymceId"></textarea>
-    </label>
-  </div>
+  <label>
+    <textarea class="tinymce-textarea" :id="tinymceId"></textarea>
+  </label>
 </template>
 
 <script>
 import util from '@/utils/util'
 import '@static/tinymce/tinymce.min'
-import { config, plugins, toolbar } from './config/config'
+import { plugins, toolbar } from './config/config'
 
 export default {
   name: 'cs-tinymce',
@@ -23,32 +21,75 @@ export default {
       type: String,
       default: undefined
     },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
     plugins: {
-      type: [String, Array],
+      type: String,
       default: plugins
     },
     toolbar: {
-      type: [String, Array],
+      type: [String, Boolean],
       default: toolbar
     },
     menubar: {
-      type: [String, Array, Boolean],
-      default: 'file edit insert view format table'
+      type: [String, Boolean],
+      default: true
     },
     height: {
       type: Number,
-      required: false,
       default: 300
+    },
+    config: {
+      type: Object,
+      default: () => {}
     }
   },
   data() {
     return {
-      myEditor: null,
-      tinymceId: this.id || util.guid()
+      handleEditor: null,
+      tinymceId: this.id || util.guid(),
+      defaultConfig: {
+        // GLOBAL
+        theme: 'modern',
+        language: 'zh_CN',
+        branding: false,
+        height: this.height,
+        menubar: this.menubar,
+        toolbar: this.toolbar,
+        plugins: this.plugins,
+        // CONFIG
+        forced_root_block: '',
+        force_p_newlines: false,
+        importcss_append: false,
+        nonbreaking_force_tab: true,
+        toolbar_items_size: 'small',
+        autosave_ask_before_unload: false,
+        content_style: 'html, body { font-family:inherit; font-size: 14px; line-height:inherit; }',
+        // IMAGE
+        image_caption: true,
+        image_advtab: true,
+        // LINK
+        default_link_target: '_blank',
+        link_context_toolbar: true,
+        // Tab
+        tabfocus_elements: ':prev,:next',
+        object_resizing: true,
+        // FontSize
+        fontsize_formats: '10px 11px 12px 14px 16px 18px 20px 24px 36px',
+        // FontSelect
+        font_formats: `
+          微软雅黑=微软雅黑;
+          宋体=宋体;
+          黑体=黑体;
+          仿宋=仿宋;
+          楷体=楷体;
+          隶书=隶书;
+          幼圆=幼圆;
+          Andale Mono=andale mono,times;
+          Arial=arial, helvetica,
+          Arial Black=arial black, avant garde;
+          Comic Sans MS=comic sans ms,sans-serif;
+          Impact=impact,chicago;
+          Times New Roman=times new roman,times`
+      }
     }
   },
   mounted() {
@@ -67,12 +108,28 @@ export default {
       window.tinymce.baseURL = '/static/tinymce'
       window.tinymce.init({
         // 默认配置
+        ...this.defaultConfig,
+        // 自定义配置
+        ...this.config,
+        // 挂载DOM对象
         selector: `#${this.tinymceId}`,
-        // language_url: '/static/tinymce/langs/zh_CN.js',
-        language: 'zh_CN',
-        // skin_url: '/static/tinymce/skins/lightgray',
-        height: this.height,
-        plugins: 'visualblocks'
+        setup: editor => {
+          self.handleEditor = editor
+          editor.on(
+            'init', () => {
+              self.loading = true
+              self.$emit('on-ready') // 抛出 'on-ready' 事件钩子
+              editor.setContent(self.value)
+              self.loading = false
+            }
+          )
+          // 抛出 'input' 事件钩子，同步value数据
+          editor.on(
+            'input change undo redo', () => {
+              self.$emit('input', editor.getContent())
+            }
+          )
+        }
       })
     },
     destroyTinymce() {
@@ -80,10 +137,10 @@ export default {
       window.tinymce.remove(`#${this.tinymceId}`)
     },
     setContent(value) {
-      this.myEditor.setContent(value)
+      this.handleEditor.setContent(value)
     },
     getContent() {
-      return this.myEditor.getContent()
+      return this.handleEditor.getContent()
     }
   },
   destroyed() {
