0ec8e12929c7e8d6b68bd21adaf4d2656f57aaab
思维空间 <dnyz520@126.com>
2019-02-12 16:58:08 +0800

    topic

--- src/views/system/article/topic/componnets/PageMain/index.vue
+++ src/views/system/article/topic/componnets/PageMain/index.vue
@@ -69,6 +69,69 @@
 
       <el-table-column type="selection" width="55"/>
 
+      <el-table-column
+        label="标题"
+        prop="title"
+        sortable="custom"
+        min-width="200"
+        :show-overflow-tooltip="true">
+      </el-table-column>
+
+      <el-table-column
+        label="别名"
+        prop="alias"
+        sortable="custom"
+        min-width="130"
+        :show-overflow-tooltip="true">
+      </el-table-column>
+
+      <el-table-column
+        label="状态"
+        prop="status"
+        sortable="custom"
+        align="center"
+        width="100">
+        <template slot-scope="scope">
+          <el-tag
+            size="mini"
+            :type="statusMap[scope.row.status].type"
+            style="cursor: pointer;"
+            @click.native="handleStatus(scope.$index)">
+            {{statusMap[scope.row.status].text}}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="创建时间"
+        prop="create_time"
+        sortable="custom"
+        align="center"
+        width="160">
+      </el-table-column>
+
+      <el-table-column
+        label="操作"
+        align="center"
+        min-width="100">
+        <template slot-scope="scope">
+          <el-button
+            size="small"
+            @click="handleView(scope.$index)"
+            type="text">预览</el-button>
+
+          <el-button
+            size="small"
+            @click="handleEdit(scope.row.topic_id)"
+            type="text">编辑</el-button>
+
+          <el-button
+            size="small"
+            @click="handleDelete(scope.$index)"
+            type="text">删除</el-button>
+        </template>
+      </el-table-column>
+
     </el-table>
   </div>
 </template>
@@ -87,7 +150,66 @@ export default {
     return {
       currentTableData: [],
       multipleSelection: [],
-      helpContent: '暂无帮助内容'
+      helpContent: '暂无帮助内容',
+      statusMap: {
+        0: {
+          text: '禁用',
+          type: 'danger'
+        },
+        1: {
+          text: '启用',
+          type: 'success'
+        },
+        2: {
+          text: '...',
+          type: 'info'
+        }
+      }
+    }
+  },
+  watch: {
+    tableData: {
+      handler(val) {
+        this.currentTableData = val
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    // 获取列表中的专题编号
+    _getTopicIdList(val) {
+      if (val === null) {
+        val = this.multipleSelection
+      }
+
+      let idList = []
+      if (Array.isArray(val)) {
+        val.forEach(value => {
+          idList.push(value.topic_id)
+        })
+      } else {
+        idList.push(this.currentTableData[val].topic_id)
+      }
+
+      return idList
+    },
+    // 选中数据项
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    // 获取排序字段
+    sortChange({ column, prop, order }) {
+      let sort = {
+        order_type: undefined,
+        order_field: undefined
+      }
+
+      if (column) {
+        sort.order_type = order === 'ascending' ? 'asc' : 'desc'
+        sort.order_field = prop
+      }
+
+      this.$emit('sort', sort)
     }
   }
 }
---
+++ src/views/system/article/topic/view.vue
@@ -0,0 +1,129 @@
+<template>
+  <cs-container
+    :model="topic_id"
+    parentPath="system-article-topic"
+    :scrollTop="scrollTop"
+    @scroll="(move) => {this.scrollTop = move.y}">
+    <el-card
+      class="box-card"
+      shadow="never"
+      v-loading="loading">
+      <div
+        slot="header"
+        class="clearfix">
+        <h2>{{topic.title}}</h2>
+        <span>最后编辑：{{topic.update_time}}</span>
+        <span>别名：{{topic.alias}}</span>
+      </div>
+      <div class="mce-content-body" v-html="topic.content"></div>
+    </el-card>
+  </cs-container>
+</template>
+
+<script>
+import { getTopicItem } from '@/api/article/topic'
+
+export default {
+  props: {
+    topic_id: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  data() {
+    return {
+      topic: {},
+      scrollTop: 0,
+      loading: false
+    }
+  },
+  mounted() {
+    this.getTopicData()
+  },
+  watch: {
+    topic_id: {
+      handler() {
+        this.getTopicData()
+      }
+    }
+  },
+  methods: {
+    resetTopicData() {
+      this.topic = {}
+    },
+    getTopicData() {
+      this.loading = true
+      this.resetTopicData()
+
+      getTopicItem(this.topic_id)
+        .then(res => {
+          this.topic = res.data
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .box-card {
+    border-radius: 0;
+  }
+  .clearfix{
+    text-align: center;
+  }
+  .clearfix span, a{
+    color: $color-text-sub;
+    font-size: 13px;
+    margin-right: 15px;
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+</style>
+
+<style scoped>
+  .mce-content-body {
+    padding: 0;
+  }
+  .mce-content-body>>>html, body {
+    font-family: inherit;
+    font-size: 14px;
+    line-height: inherit;
+  }
+  .mce-content-body>>>img {
+    max-width: 100%;
+    display: block;
+    height: auto;
+  }
+  .mce-content-body>>>a {
+    text-decoration: none;
+  }
+  .mce-content-body>>>iframe {
+    width: 100%;
+  }
+  .mce-content-body>>>p {
+    line-height: 1.6;
+  }
+  .mce-content-body>>>table {
+    word-wrap: break-word;
+    word-break: break-all;
+    max-width: 100%;
+    border: none #999;
+  }
+  .mce-content-body>>>.mce-object-iframe{
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0;
+  }
+  .mce-content-body>>>ul, ol{
+    list-style-position: inside;
+  }
+</style>
