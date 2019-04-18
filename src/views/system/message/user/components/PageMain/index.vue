a806bf78097ddfecece36dbc7b69f3f5a9c075d4
思维空间 <dnyz520@126.com>
2019-04-18 17:10:45 +0800

    up

--- src/views/system/message/user/components/PageMain/index.vue
+++ src/views/system/message/user/components/PageMain/index.vue
@@ -1,10 +1,120 @@
 <template>
   <div class="cs-p">
+    <el-form
+      :inline="true"
+      size="small">
+      <el-form-item>
+        <el-radio-group v-model="form.is_read" :disabled="loading">
+          <el-radio-button :label="null">全部</el-radio-button>
+          <el-radio-button label="0">未读</el-radio-button>
+          <el-radio-button label="1">已读</el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            标记已读
+          </el-button>
+
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            全部已读
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            批量删除
+          </el-button>
+
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            全部删除
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="$emit('refresh')">
+            刷新
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+    </el-form>
+
+    <el-tabs
+      v-model="tabPane"
+      v-loading="loading"
+      type="border-card"
+      style="box-shadow: 0 0;">
+      <el-tab-pane
+        v-for="(item, index) in typeData"
+        :key="index"
+        :label="item.name"
+        :name="index.toString()">
+        {{item.name}}
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    loading: {
+      default: false
+    },
+    tableData: {
+      default: () => []
+    },
+    unreadData: {
+      default: () => {}
+    },
+    typeData: {
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      tabPane: 0,
+      form: {
+        type: null,
+        is_read: null
+        // TODO: 刷新存在问题
+      }
+    }
+  },
+  watch: {
+    form: {
+      handler(val) {
+        this.form = val
+        this.$emit('submit', this.form, true)
+      },
+      deep: true
+    },
+    tabPane: {
+      handler(index) {
+        if (!this.typeData.hasOwnProperty(index)) {
+          return
+        }
+
+        const tabType = this.typeData[index]
+        this.form.type = tabType.value !== 'all' ? tabType.value : null
+      }
+    }
+  }
 }
 </script>
 
--- src/views/system/message/user/index.vue
+++ src/views/system/message/user/index.vue
@@ -2,21 +2,34 @@
   <cs-container
     :scrollTop="scrollTop"
     @scroll="(move) => {this.scrollTop = move.y}">
+
+    <page-main
+      :loading="loading"
+      :table-data="table"
+      :unread-data="unread"
+      :type-data="type"
+      @sort="handleSort"
+      @refresh="handleRefresh"
+      @submit="handleSubmit"/>
+
     <page-footer
       slot="footer"
       :loading="loading"
       :current="page.current"
       :size="page.size"
       :total="page.total"
-      @change="() => {}"/>
+      @change="handlePaginationChange"/>
   </cs-container>
 </template>
 
 <script>
+import { getMessageType } from '@/api/public'
+import { getMessageUserList } from '@/api/message/message'
+
 export default {
   name: 'system-message-user',
   components: {
-    // 'PageMain': () => import('./components/PageMain'),
+    'PageMain': () => import('./components/PageMain'),
     'PageFooter': () => import('@/layout/header-aside/components/footer')
   },
   data() {
@@ -24,6 +37,13 @@ export default {
       scrollTop: 0,
       loading: true,
       table: [],
+      unread: {},
+      type: [],
+      form: {
+        type: undefined,
+        is_read: undefined,
+        is_unread: 1
+      },
       page: {
         current: 1,
         size: 25,
@@ -36,8 +56,76 @@ export default {
     }
   },
   mounted() {
+    getMessageType()
+      .then(res => {
+        this.type.unshift({
+          name: '全部消息',
+          value: 'all'
+        })
+
+        for (const index in res) {
+          if (!res.hasOwnProperty(index)) {
+            continue
+          }
+
+          this.type.push({
+            name: res[index],
+            value: index
+          })
+        }
+      })
+      .then(() => {
+        this.handleSubmit()
+      })
   },
   methods: {
+    // 刷新列表页面
+    handleRefresh() {
+      this.$nextTick(() => {
+        this.handleSubmit()
+      })
+    },
+    // 分页变化改动
+    handlePaginationChange(val) {
+      this.page = val
+      this.$nextTick(() => {
+        this.handleSubmit()
+      })
+    },
+    // 排序刷新
+    handleSort(val) {
+      this.order = val
+      this.$nextTick(() => {
+        this.handleSubmit()
+      })
+    },
+    // 提交查询请求
+    handleSubmit(form, isRestore = false) {
+      if (isRestore) {
+        this.page.current = 1
+      }
+
+      this.loading = true
+      this.form = {
+        ...form,
+        is_unread: 1
+      }
+
+      getMessageUserList({
+        ...this.form,
+        ...this.order,
+        page_no: this.page.current,
+        page_size: this.page.size
+      })
+        .then(res => {
+          this.page.total = res.data.total_result
+          this.table = res.data.total_result > 0 ? res.data.items : []
+          this.unread = res.data.unread_count || {}
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
   }
 }
 </script>
