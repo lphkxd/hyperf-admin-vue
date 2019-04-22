6cee699c00957ccbdb51390d560b4ff87a49be72
思维空间 <dnyz520@126.com>
2019-04-23 00:03:17 +0800

    no message

---
+++ src/api/aided/friendlink.js
@@ -0,0 +1,118 @@
+import request from '@/utils/request'
+
+/**
+ * 添加一个友情链接
+ * @param {Object} data
+ */
+export function addFriendlinkItem(data) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'add.friendlink.item'
+    },
+    data
+  })
+}
+
+/**
+ * 编辑一个友情链接
+ * @param {Object} data
+ */
+export function setFriendlinkItem(data) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'set.friendlink.item'
+    },
+    data
+  })
+}
+
+/**
+ * 获取一个友情链接
+ * @param {Number} friend_link_id
+ */
+export function getFriendlinkItem(friend_link_id) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'get.friendlink.item'
+    },
+    data: {
+      friend_link_id
+    }
+  })
+}
+
+/**
+ * 获取友情链接列表
+ * @param {Object} data
+ */
+export function getFriendlinkList(data) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'get.friendlink.list'
+    },
+    data
+  })
+}
+
+/**
+ * 批量删除友情链接
+ * @param {Array} friend_link_id
+ */
+export function delFriendlinkList(friend_link_id) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'del.friendlink.list'
+    },
+    data: {
+      friend_link_id
+    }
+  })
+}
+
+/**
+ * 批量设置友情链接状态
+ * @param {Array} friend_link_id
+ * @param {Number} status
+ */
+export function setFriendlinkStatus(friend_link_id, status) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'set.friendlink.status'
+    },
+    data: {
+      friend_link_id,
+      status
+    }
+  })
+}
+
+/**
+ * 设置友情链接排序
+ * @param {Number} friend_link_id
+ * @param {Number} sort
+ */
+export function setFriendlinkSort(friend_link_id, sort) {
+  return request({
+    url: '/v1/friend_link',
+    method: 'post',
+    params: {
+      method: 'set.friendlink.sort'
+    },
+    data: {
+      friend_link_id,
+      sort
+    }
+  })
+}
