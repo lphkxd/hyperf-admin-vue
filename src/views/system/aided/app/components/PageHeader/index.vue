e355bdd23d18886b678b8ba6531afbe8cc92c933
思维空间 <dnyz520@126.com>
2019-04-23 14:46:59 +0800

    up

--- src/views/system/aided/app/components/PageHeader/index.vue
+++ src/views/system/aided/app/components/PageHeader/index.vue
@@ -1,13 +1,72 @@
 <template>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+    <el-form-item label="名称" prop="app_name">
+      <el-input
+        v-model="form.app_name"
+        clearable
+        placeholder="可输入应用名称"
+        @keyup.enter.native="handleFormSubmit"/>
+    </el-form-item>
 
+    <el-form-item label="状态" prop="status">
+      <el-select
+        v-model="form.status"
+        clearable
+        placeholder="请选择"
+        style="width: 120px;"
+        value="">
+        <el-option label="启用" value="1"/>
+        <el-option label="禁用" value="0"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        :disabled="loading"
+        @click="handleFormSubmit(true)">
+        <cs-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <cs-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    }
+  },
+  data() {
+    return {
+      form: {
+        app_name: undefined,
+        status: undefined
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit() {
+      this.$emit('submit', this.form)
+    },
+    handleFormReset() {
+      this.$refs.form.resetFields()
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
