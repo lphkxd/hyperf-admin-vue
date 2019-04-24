55c869c7e59a76271340a400d04a2a5a8b37a1b3
思维空间 <dnyz520@126.com>
2019-04-24 17:57:39 +0800

    up

--- src/views/system/aided/qrcode/components/PageHeader/index.vue
+++ src/views/system/aided/qrcode/components/PageHeader/index.vue
@@ -1,13 +1,75 @@
 <template>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+    <el-form-item label="名称" prop="name">
+      <el-input
+        v-model="form.name"
+        prefix-icon="el-icon-search"
+        clearable
+        placeholder="可输入二维码名称"
+        @keyup.enter.native="handleFormSubmit"/>
+    </el-form-item>
 
+    <el-form-item label="尺寸" prop="size">
+      <el-select
+        v-model="form.size"
+        clearable
+        placeholder="请选择"
+        style="width: 120px;"
+        value="">
+        <el-option
+          v-for="index in 10"
+          :key="index"
+          :value="index"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        :disabled="loading"
+        @click="handleFormSubmit(true)">
+        <cs-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <cs-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    }
+  },
+  data() {
+    return {
+      form: {
+        name: undefined,
+        size: undefined
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit(isRestore = false) {
+      this.$emit('submit', this.form, isRestore)
+    },
+    handleFormReset() {
+      this.$refs.form.resetFields()
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/views/system/aided/qrcode/components/PageMain/index.vue
+++ src/views/system/aided/qrcode/components/PageMain/index.vue
@@ -1,13 +1,50 @@
 <template>
+  <div class="cs-p">
+    <el-form
+      :inline="true"
+      size="small">
 
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="handleCreate">
+            <cs-icon name="plus"/>
+            新增二维码
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="handleDelete(null)">
+            <cs-icon name="trash-o"/>
+            删除
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-popover
+        style="float: right"
+        placement="bottom-end"
+        width="400"
+        trigger="hover"
+        title="提示"
+        @show="getHelp">
+        <div class="popover-content" v-html="helpContent"></div>
+        <el-button
+          size="small"
+          slot="reference">
+          <cs-icon name="question"/>
+        </el-button>
+      </el-popover>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
 }
 </script>
-
-<style scoped>
-
-</style>
