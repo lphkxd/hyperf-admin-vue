1db73e30af0397fc83935dfc4f2681825c633061
思维空间 <dnyz520@126.com>
2019-04-26 01:23:28 +0800

    up

--- src/views/system/aided/qrcode/components/PageMain/index.vue
+++ src/views/system/aided/qrcode/components/PageMain/index.vue
@@ -54,19 +54,15 @@
 
       <el-table-column
         label="名称"
-        prop="name">
+        prop="name"
+        min-width="180">
       </el-table-column>
 
       <el-table-column
         label="内容"
-        prop="text">
-      </el-table-column>
-
-      <el-table-column
-        label="尺寸"
-        prop="size"
-        sortable="custom"
-        width="80">
+        prop="text"
+        min-width="220"
+        :show-overflow-tooltip="true">
       </el-table-column>
 
       <el-table-column
@@ -85,12 +81,19 @@
       </el-table-column>
 
       <el-table-column
+        label="尺寸"
+        prop="size"
+        sortable="custom"
+        width="75">
+      </el-table-column>
+
+      <el-table-column
         label="操作"
         align="center"
         min-width="100">
         <template slot-scope="scope">
           <el-button
-            @click="handleUpdate(scope.$index)"
+            @click="() => {}"
             size="small"
             type="text">详细</el-button>
 
@@ -106,16 +109,115 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <cs-upload
+      style="display: none"
+      ref="upload"
+      type="slot"
+      accept="image/*"
+      :limit="1"
+      :multiple="false"
+      @confirm="_getUploadFileList"/>
+
+    <el-dialog
+      :title="textMap[dialogStatus]"
+      :visible.sync="dialogFormVisible"
+      :append-to-body="true"
+      width="600px">
+      <el-form
+        :model="form"
+        :rules="rules"
+        ref="form"
+        label-width="80px">
+        <el-form-item
+          label="名称"
+          prop="name">
+          <el-input
+            v-model="form.name"
+            placeholder="请输入二维码名称"
+            clearable/>
+        </el-form-item>
+
+        <el-form-item
+          label="内容"
+          prop="text">
+          <el-input
+            v-model="form.text"
+            type="textarea"
+            :rows="2"
+            placeholder="可输入二维码内容"
+            clearable/>
+        </el-form-item>
+
+        <el-form-item
+          label="Logo"
+          prop="logo">
+          <el-input
+            v-model="form.logo"
+            placeholder="可输入二维码Logo链接"
+            clearable>
+            <el-button
+              slot="append"
+              @click="$refs.upload.handleUploadDlg()"
+              icon="el-icon-upload"></el-button>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item
+          label="尺寸"
+          prop="size">
+          <el-input-number
+            v-model="form.size"
+            controls-position="right"
+            :min="1"
+            :max="10"
+            label="可输入二维码图片尺寸"
+            style="width: 120px;"/>
+        </el-form-item>
+
+        <cs-line></cs-line>
+
+        <el-form-item
+          label="效果预览">
+          <img v-if="qrcodeImage" :src="qrcodeImage" alt="">
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button
+          @click="dialogFormVisible = false"
+          size="small">取消</el-button>
+
+        <el-button
+          v-if="dialogStatus === 'create'"
+          type="primary"
+          :loading="dialogLoading"
+          @click="create"
+          size="small">确定</el-button>
+
+        <el-button
+          v-else type="primary"
+          :loading="dialogLoading"
+          @click="update"
+          size="small">修改</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import {
-  delQrcodeList
+  getQrcodeCallurl,
+  delQrcodeList,
+  addQrcodeItem,
+  setQrcodeItem
 } from '@/api/aided/qrcode'
 import { getHelpRouter } from '@/api/index/help'
 
 export default {
+  components: {
+    'csUpload': () => import('@/components/cs-upload')
+  },
   props: {
     loading: {
       default: false
@@ -126,6 +228,8 @@ export default {
   },
   data() {
     return {
+      qrcodeUrl: '',
+      qrcodeImage: '',
       currentTableData: [],
       multipleSelection: [],
       helpContent: '',
@@ -144,6 +248,49 @@ export default {
         logo: undefined
       },
       rules: {
+        name: [
+          {
+            required: true,
+            message: '名称不能为空',
+            trigger: 'blur'
+          },
+          {
+            max: 50,
+            message: '长度不能大于 50 个字符',
+            trigger: 'blur'
+          }
+        ],
+        text: [
+          {
+            max: 255,
+            message: '内容不能大于 255 个字符',
+            trigger: 'blur'
+          }
+        ],
+        logo: [
+          {
+            max: 255,
+            message: 'Logo不能大于 255 个字符',
+            trigger: 'blur'
+          }
+        ],
+        size: [
+          {
+            type: 'number',
+            message: '必须为数字值',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    changeQrcode() {
+      const { text, size, logo } = this.form
+      return {
+        text,
+        size,
+        logo
       }
     }
   },
@@ -153,10 +300,21 @@ export default {
         this.currentTableData = val
       },
       immediate: true
+    },
+    changeQrcode: {
+      handler() {
+        this.$nextTick(() => {
+          this._getQrcodeImage(null)
+        })
+      }
     }
   },
   mounted() {
     this._validationAuth()
+    getQrcodeCallurl()
+      .then(res => {
+        this.qrcodeUrl = res.data['call_url']
+      })
   },
   methods: {
     // 验证权限
@@ -178,6 +336,20 @@ export default {
       }
 
       return idList
+    },
+    // 获取二维码预览
+    _getQrcodeImage(val) {
+      let parm = val || `?text=${this.form.text}&size=${this.form.size}&logo=${this.form.logo}`
+      this.qrcodeImage = this.qrcodeUrl + encodeURI(parm)
+    },
+    // 获取上传文件
+    _getUploadFileList(files) {
+      const response = files[0].response
+      if (!response || response.status !== 200) {
+        return
+      }
+
+      this.form.logo = document.location.protocol + '//' + response.data[0].url
     },
     // 获取帮助文档
     getHelp() {
@@ -206,9 +378,38 @@ export default {
     },
     // 弹出新建对话框
     handleCreate() {
+      this.qrcodeImage = ''
+      this.form = {
+        name: '',
+        text: '',
+        size: 3,
+        logo: ''
+      }
+
+      this.$nextTick(() => {
+        this.$refs.form.clearValidate()
+      })
+
+      this.dialogStatus = 'create'
+      this.dialogLoading = false
+      this.dialogFormVisible = true
     },
     // 请求创建
     create() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.dialogLoading = true
+          addQrcodeItem(this.form)
+            .then(res => {
+              this.currentTableData.unshift(res.data)
+              this.dialogFormVisible = false
+              this.$message.success('操作成功')
+            })
+            .catch(() => {
+              this.dialogLoading = false
+            })
+        }
+      })
     },
     // 批量删除
     handleDelete(val) {
@@ -240,9 +441,43 @@ export default {
     },
     // 编辑二维码
     handleUpdate(index) {
+      this.qrcodeImage = ''
+      this.currentIndex = index
+      this.form = { ...this.currentTableData[index] }
+
+      if (this.$refs.form) {
+        this.$nextTick(() => {
+          this.$refs.form.clearValidate()
+        })
+      }
+
+      this.dialogStatus = 'update'
+      this.dialogLoading = false
+      this.dialogFormVisible = true
     },
     // 请求编辑
     update() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.dialogLoading = true
+          setQrcodeItem(this.form)
+            .then(res => {
+              this.$set(
+                this.currentTableData,
+                this.currentIndex,
+                {
+                  ...this.currentTableData[this.currentIndex],
+                  ...res.data
+                })
+
+              this.dialogFormVisible = false
+              this.$message.success('操作成功')
+            })
+            .catch(() => {
+              this.dialogLoading = false
+            })
+        }
+      })
     }
   }
 }
