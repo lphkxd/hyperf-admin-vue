106ffa78245c778ed95b7a5f43741beb743eec63
思维空间 <dnyz520@126.com>
2019-04-25 16:58:17 +0800

    up

--- src/views/system/aided/qrcode/components/PageMain/index.vue
+++ src/views/system/aided/qrcode/components/PageMain/index.vue
@@ -53,11 +53,6 @@
       <el-table-column type="selection" width="55"/>
 
       <el-table-column
-        label="ID"
-        prop="qrcode_id">
-      </el-table-column>
-
-      <el-table-column
         label="名称"
         prop="name">
       </el-table-column>
@@ -69,19 +64,36 @@
 
       <el-table-column
         label="尺寸"
-        prop="size">
+        prop="size"
+        sortable="custom"
+        width="80">
       </el-table-column>
 
       <el-table-column
         label="Logo"
-        prop="logo">
+        prop="logo"
+        align="center">
+        <template slot-scope="scope">
+          <el-popover
+            v-if="scope.row.logo"
+            placement="top"
+            trigger="hover">
+            <img class="image" :src="scope.row.logo" alt="">
+            <cs-icon slot="reference" name="image"/>
+          </el-popover>
+        </template>
       </el-table-column>
 
       <el-table-column
         label="操作"
         align="center"
-        min-width="90">
+        min-width="100">
         <template slot-scope="scope">
+          <el-button
+            @click="handleUpdate(scope.$index)"
+            size="small"
+            type="text">详细</el-button>
+
           <el-button
             @click="handleUpdate(scope.$index)"
             size="small"
@@ -98,6 +110,9 @@
 </template>
 
 <script>
+import {
+  delQrcodeList
+} from '@/api/aided/qrcode'
 import { getHelpRouter } from '@/api/index/help'
 
 export default {
@@ -122,8 +137,14 @@ export default {
         update: '编辑二维码',
         create: '新增二维码'
       },
-      form: {},
-      rules: {}
+      form: {
+        name: undefined,
+        text: undefined,
+        size: undefined,
+        logo: undefined
+      },
+      rules: {
+      }
     }
   },
   watch: {
@@ -133,6 +154,104 @@ export default {
       },
       immediate: true
     }
+  },
+  mounted() {
+    this._validationAuth()
+  },
+  methods: {
+    // 验证权限
+    _validationAuth() {
+    },
+    // 获取列表中的编号
+    _getIdList(val) {
+      if (val === null) {
+        val = this.multipleSelection
+      }
+
+      let idList = []
+      if (Array.isArray(val)) {
+        val.forEach(value => {
+          idList.push(value.qrcode_id)
+        })
+      } else {
+        idList.push(this.currentTableData[val].qrcode_id)
+      }
+
+      return idList
+    },
+    // 获取帮助文档
+    getHelp() {
+      if (!this.helpContent) {
+        this.helpContent = '正在获取内容,请稍后...'
+        getHelpRouter(this.$route.path).then(res => { this.helpContent = res })
+      }
+    },
+    // 选中数据项
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    // 获取排序字段
+    sortChange({ column, prop, order }) {
+      let sort = {
+        order_type: undefined,
+        order_field: undefined
+      }
+
+      if (column) {
+        sort.order_type = order === 'ascending' ? 'asc' : 'desc'
+        sort.order_field = prop
+      }
+
+      this.$emit('sort', sort)
+    },
+    // 弹出新建对话框
+    handleCreate() {
+    },
+    // 请求创建
+    create() {
+    },
+    // 批量删除
+    handleDelete(val) {
+      let qrcode_id = this._getIdList(val)
+      if (qrcode_id.length === 0) {
+        this.$message.error('请选择要操作的数据')
+        return
+      }
+
+      this.$confirm('确定要执行该操作吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          delQrcodeList(qrcode_id)
+            .then(() => {
+              for (let i = this.currentTableData.length - 1; i >= 0; i--) {
+                if (qrcode_id.indexOf(this.currentTableData[i].qrcode_id) !== -1) {
+                  this.currentTableData.splice(i, 1)
+                }
+              }
+
+              this.$message.success('操作成功')
+            })
+        })
+        .catch(() => {
+        })
+    },
+    // 编辑二维码
+    handleUpdate(index) {
+    },
+    // 请求编辑
+    update() {
+    }
   }
 }
 </script>
+
+<style scoped>
+  .image {
+    max-width: 150px;
+    margin: 0 auto;
+    display: block;
+  }
+</style>
