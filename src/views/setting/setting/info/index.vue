31254d5c6cd595f605705bde34bcdd2fb1c1d955
思维空间 <dnyz520@126.com>
2019-05-09 00:03:38 +0800

    up

--- src/views/setting/setting/info/index.vue
+++ src/views/setting/setting/info/index.vue
@@ -1,5 +1,26 @@
 <template>
   <cs-container>
+    <el-form
+      v-loading="loading"
+      class="form-box cs-m"
+      label-width="150px"
+      label-position="left">
+      <el-form-item :label="version.name">
+        <span>{{version.value}}</span>
+      </el-form-item>
+
+      <el-form-item :label="admin.name">
+        <span>{{admin.value}}</span>
+      </el-form-item>
+
+      <el-form-item :label="delivery.name">
+        <span>{{delivery.value}}</span>
+      </el-form-item>
+
+      <el-form-item :label="qrcode.name">
+        <span>{{qrcode.value}}</span>
+      </el-form-item>
+    </el-form>
   </cs-container>
 </template>
 
@@ -10,7 +31,22 @@ export default {
   name: 'setting-setting-info',
   data() {
     return {
-      form: [],
+      version: {
+        name: '框架内核系统版本号',
+        value: '#'
+      },
+      admin: {
+        name: '后台管理系统版本号',
+        value: process.env.VUE_APP_VERSION
+      },
+      delivery: {
+        name: '配送回调URL',
+        value: '#'
+      },
+      qrcode: {
+        name: '二维码调用URL',
+        value: '#'
+      },
       loading: false
     }
   },
@@ -34,12 +70,26 @@ export default {
       {
         version: 'v1',
         controller: 'payment',
-        method: 'get.payment.notify'
+        method: 'get.payment.notify',
+        to_payment: 2
       },
       {
         version: 'v1',
         controller: 'payment',
-        method: 'get.payment.return'
+        method: 'get.payment.return',
+        to_payment: 2
+      },
+      {
+        version: 'v1',
+        controller: 'payment',
+        method: 'get.payment.notify',
+        to_payment: 3
+      },
+      {
+        version: 'v1',
+        controller: 'payment',
+        method: 'get.payment.return',
+        to_payment: 3
       }
     ]
 
@@ -54,3 +104,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .form-box {
+    padding: 20px;
+    background-color: #FFF;
+  }
+</style>
