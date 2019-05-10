81a2bd50469a12a9cc742883357acabd4481897f
思维空间 <dnyz520@126.com>
2019-05-10 18:05:40 +0800

    temp up

--- src/views/setting/setting/info/index.vue
+++ src/views/setting/setting/info/index.vue
@@ -20,6 +20,19 @@
       <el-form-item :label="qrcode.name">
         <span>{{qrcode.value}}</span>
       </el-form-item>
+
+      <el-form-item :label="alipay.notify.name">
+        <span>{{alipay.notify.value}}</span>
+      </el-form-item>
+
+      <el-form-item :label="alipay.return.name">
+        <span>{{alipay.return.value}}</span>
+      </el-form-item>
+
+      <el-form-item label="微信回调">
+        <p>{{weixin.notify.name}} {{weixin.notify.value}}</p>
+        <p>{{weixin.return.name}} {{weixin.return.value}}</p>
+      </el-form-item>
     </el-form>
   </cs-container>
 </template>
@@ -32,20 +45,40 @@ export default {
   data() {
     return {
       version: {
-        name: '框架内核系统版本号',
-        value: '#'
+        name: '框架内核版本号',
+        value: ''
       },
       admin: {
-        name: '后台管理系统版本号',
+        name: '后台管理版本号',
         value: process.env.VUE_APP_VERSION
       },
       delivery: {
         name: '配送回调URL',
-        value: '#'
+        value: ''
       },
       qrcode: {
         name: '二维码调用URL',
-        value: '#'
+        value: ''
+      },
+      alipay: {
+        notify: {
+          name: '支付宝异步(notify)URL',
+          value: ''
+        },
+        return: {
+          name: '支付宝同步(return)URL',
+          value: ''
+        }
+      },
+      weixin: {
+        notify: {
+          name: '微信异步(notify)URL',
+          value: ''
+        },
+        return: {
+          name: '微信同步(return)URL',
+          value: ''
+        }
       },
       loading: false
     }
@@ -96,7 +129,28 @@ export default {
     this.loading = true
     batchRequests(data)
       .then(res => {
-        console.log(res)
+        res.data.forEach(value => {
+          console.log(value)
+          if (value.status === 200) {
+            switch (value.method) {
+              case 'get.system.version':
+                this.version.value = value.data.version
+                break
+
+              case 'get.delivery.dist.callback':
+                this.delivery.value = value.data.callback_url
+                break
+
+              case 'get.qrcode.callurl':
+                this.qrcode.value = value.data.call_url
+                break
+
+              case 'get.payment.notify':
+                if (value.data.to_payment) {
+                }
+            }
+          }
+        })
       })
       .finally(() => {
         this.loading = false
