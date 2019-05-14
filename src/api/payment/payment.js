6202bafd5aa3c46a982c9d533071533d0e4398db
思维空间 <dnyz520@126.com>
2019-05-14 10:08:55 +0800

    no message

--- src/api/payment/payment.js
+++ src/api/payment/payment.js
@@ -15,3 +15,77 @@ export function getPaymentList(data) {
     data
   })
 }
+
+/**
+ * 编辑一个支付配置
+ * @param {Object} data
+ * @returns {AxiosPromise}
+ */
+export function setPaymentItem(data) {
+  return request({
+    url: '/v1/payment',
+    method: 'post',
+    params: {
+      method: 'set.payment.item'
+    },
+    data
+  })
+}
+
+/**
+ * 获取一个支付配置
+ * @param {Number} payment_id
+ * @returns {AxiosPromise}
+ */
+export function getPaymentItem(payment_id) {
+  return request({
+    url: '/v1/payment',
+    method: 'post',
+    params: {
+      method: 'get.payment.item'
+    },
+    data: {
+      payment_id
+    }
+  })
+}
+
+/**
+ * 设置支付配置排序
+ * @param {Number} payment_id
+ * @param {Number} sort
+ * @returns {AxiosPromise}
+ */
+export function setPaymentSort(payment_id, sort) {
+  return request({
+    url: '/v1/payment',
+    method: 'post',
+    params: {
+      method: 'set.payment.sort'
+    },
+    data: {
+      payment_id,
+      sort
+    }
+  })
+}
+
+/**
+ * 批量设置支付配置状态
+ * @param {Array} payment_id
+ * @param {Number} status
+ * @returns {AxiosPromise}
+ */
+export function setPaymentStatus(payment_id, status) {
+  return request({
+    url: '/v1/payment',
+    method: 'post',
+    params: {
+      method: 'set.payment.status'
+    },
+    data: {
+      payment_id,
+      status
+    }
+  })
+}
