03c1f2d79e2f61fe1d78cc035ec5728232610431
思维空间 <dnyz520@126.com>
2019-05-29 17:52:22 +0800

    up

--- src/components/cs-tinymce/config/config.js
+++ src/components/cs-tinymce/config/config.js
@@ -1,7 +1,7 @@
 // Any plugins you want to use has to be imported
 // Detail plugins list see https://www.tinymce.com/docs/plugins/
 // Custom builds see https://www.tinymce.com/download/custom-builds/
-export const plugins = 'advlist anchor autolink autosave charmap code codesample colorpicker contextmenu directionality emoticons fullscreen hr image imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print searchreplace tabfocus table textcolor textpattern visualblocks visualchars wordcount'
+export const plugins = 'advlist anchor autolink autosave charmap code codesample colorpicker contextmenu directionality emoticons fullscreen hr image insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print searchreplace tabfocus table textcolor textpattern visualblocks visualchars wordcount'
 
 // Here is a list of the toolbar
 // Detail list see https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
