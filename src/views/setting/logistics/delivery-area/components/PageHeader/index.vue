a84d7003d3ace075884fe1a069c596063037ff9e
思维空间 <dnyz520@126.com>
2019-06-04 17:36:02 +0800

    no message

---
+++ src/views/setting/logistics/delivery-area/components/PageHeader/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-page-header @back="goBack">
+    <template slot="content">
+      <span>{{title}}</span>
+    </template>
+  </el-page-header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  methods: {
+    goBack() {
+      this.$router.push({
+        name: 'setting-logistics-delivery'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/setting/logistics/delivery-area/index.vue
+++ src/views/setting/logistics/delivery-area/index.vue
@@ -1,16 +1,43 @@
 <template>
-  <div>
-    {{$route.params.name}}<br>
-    {{$route.params.delivery_id}}
-  </div>
+  <cs-container :is-back-to-top="true" parent-path="setting-logistics-delivery">
+    <page-header
+      slot="header"
+      :title="title"/>
+
+    <page-main
+      :table-data="table"
+      :loading="loading"/>
+  </cs-container>
 </template>
 
 <script>
+import { getDeliveryAreaList } from '@/api/logistics/area'
+import { getDeliveryCompanyItem } from '@/api/logistics/company'
+
 export default {
-  name: 'index'
+  name: 'setting-logistics-delivery-area',
+  components: {
+    'PageHeader': () => import('./components/PageHeader'),
+    'PageMain': () => import('./components/PageMain')
+  },
+  computed: {
+    change() {
+      const { delivery_id, delivery_item_id } = this.$route.params
+      return {
+        delivery_id,
+        delivery_item_id
+      }
+    }
+  },
+  data() {
+    return {
+      // 加载状态
+      loading: true,
+      // 头部标题
+      title: '',
+      // 表格数据
+      table: []
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
