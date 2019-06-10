0ae1a387afef7c4e053ca77a98fe733f14e3b8a3
思维空间 <dnyz520@126.com>
2019-06-10 17:11:14 +0800

    up

--- src/views/setting/logistics/delivery-area/components/PageMain/components/cs-region/index.vue
+++ src/views/setting/logistics/delivery-area/components/PageMain/components/cs-region/index.vue
@@ -8,18 +8,61 @@
       <el-tag
         v-for="item in value"
         :key="item.region_id"
-        class="cs-mr-10"
+        :disable-transitions="true"
+        class="region-tag"
         effect="plain"
         type="info"
         @close="handleClose(item)"
         closable>
         {{item.name}}
       </el-tag>
+
+      <div v-if="value.length <= 0" class="not-region">
+        <span>暂无区域</span>
+      </div>
+
+      <el-divider>区域选择</el-divider>
+      <div style="height: 300px; overflow: auto;">
+        <el-tree
+          v-if="visible"
+          v-loading="treeLoading"
+          node-key="region_id"
+          :props="treeProps"
+          :load="loadNode"
+          lazy>
+          <span class="custom-tree-node action" slot-scope="{ node, data }">
+            <span>{{node.label}}</span>
+
+            <span class="active">
+              <el-button
+                type="text"
+                size="mini"
+                @click.stop="() => addRegion(data)"
+                icon="el-icon-plus">加入到所辖区域</el-button>
+            </span>
+          </span>
+        </el-tree>
+      </div>
+    </div>
+
+    <div slot="footer" class="dialog-footer">
+      <el-button
+        @click="visible = false"
+        size="small">取消</el-button>
+
+      <el-button
+        type="primary"
+        :loading="loading"
+        @click="update"
+        size="small">确定</el-button>
     </div>
   </el-dialog>
 </template>
 
 <script>
+import { getRegionList } from '@/api/logistics/region'
+import { setDeliveryAreaItem } from '@/api/logistics/area'
+
 export default {
   props: {
     // 外部v-model值
@@ -37,7 +80,13 @@ export default {
     return {
       areaId: 0,
       visible: false,
-      loading: false
+      loading: false,
+      regionId: 1,
+      treeLoading: false,
+      treeProps: {
+        label: 'region_name',
+        children: 'children'
+      }
     }
   },
   methods: {
@@ -50,7 +99,75 @@ export default {
     // 移除一个区域
     handleClose(tag) {
       this.value.splice(this.value.indexOf(tag), 1)
+    },
+    // 动态加载节点
+    loadNode(node, resolve) {
+      this.treeLoading = node.level === 0
+      this.getRegionData(node.level ? node.key : this.regionId, resolve)
+    },
+    // 获取节点
+    getRegionData(regionId, resolve) {
+      getRegionList({ region_id: regionId })
+        .then(res => {
+          const data = res.data.length ? res.data : []
+          resolve(data)
+        })
+        .finally(() => {
+          this.treeLoading = false
+        })
+    },
+    // 添加区域
+    addRegion(value) {
+      const node = this.value.find(item => item.region_id === value.region_id)
+      if (node) {
+        this.$message.warning(`${node.name} 在所辖区域中已存在`)
+        return
+      }
+
+      this.value.push({
+        name: value.region_name,
+        region_id: value.region_id
+      })
+    },
+    // 修改所辖区域
+    update() {
+      this.loading = true
+      setDeliveryAreaItem({
+        delivery_area_id: this.areaId,
+        region: this.value.map(item => { return item.region_id })
+      })
+        .then(res => {
+          this.$emit('confirm', res.data.region)
+          this.visible = false
+        })
+        .finally(() => {
+          this.loading = false
+        })
     }
   }
 }
 </script>
+
+<style scoped>
+  .region-tag {
+    margin: 0 10px 10px 0;
+  }
+  .not-region {
+    color: #99a9bf;
+    text-align: center;
+  }
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+  .active {
+    display: none;
+  }
+  .action:hover .active{
+    display: block;
+  }
+</style>
