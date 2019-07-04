bbb5fe0834c882a56ef17cae3952ba99126f982b
思维空间 <dnyz520@126.com>
2019-07-04 23:41:18 +0800

    up

--- src/router/modules/goods.js
+++ src/router/modules/goods.js
@@ -11,6 +11,12 @@ export default {
       name: `${pre}index`,
       meta: { ...meta, cache: true, title: '商品首页' },
       component: () => import('@/views/goods/index')
+    },
+    {
+      path: 'setting/category',
+      name: `${pre}setting-category`,
+      meta: { ...meta, cache: true, title: '商品分类' },
+      component: () => import('@/views/goods/category')
     }
   ])('goods-')
 }
