022e23cdcf15f2d1f81a5c3cc7b9538bd168fa09
思维空间 <dnyz520@126.com>
2019-07-17 01:00:21 +0800

    up

--- src/views/goods/attribute/components/PageHeader/index.vue
+++ src/views/goods/attribute/components/PageHeader/index.vue
@@ -1,13 +1,80 @@
 <template>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    @submit.native.prevent
+    style="margin-bottom: -18px;">
+    <el-form-item label="所属模型" prop="goods_type_id">
+      <el-select
+        v-model="form.goods_type_id"
+        placeholder="请选择"
+        clearable
+        value="">
+        <el-option
+          v-for="(item, index) in typeData"
+          :key="index"
+          :label="item.type_name"
+          :value="item.goods_type_id"/>
+      </el-select>
+    </el-form-item>
 
+    <el-form-item>
+      <el-button
+        type="primary"
+        :disabled="loading"
+        @click="handleFormSubmit(true)">
+        <cs-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <cs-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    },
+    typeId: {
+      default: null
+    },
+    typeData: {
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      form: {
+        goods_type_id: undefined
+      }
+    }
+  },
+  watch: {
+    typeId: {
+      handler(val) {
+        this.form.goods_type_id = val
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    handleFormSubmit(isRestore = false) {
+      this.$emit('submit', this.form, isRestore)
+    },
+    handleFormReset() {
+      this.form.goods_type_id = undefined
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/views/goods/attribute/components/PageMain/index.vue
+++ src/views/goods/attribute/components/PageMain/index.vue
@@ -1,13 +1,156 @@
 <template>
+  <div class="cs-p">
+    <el-form
+      :inline="true"
+      size="small">
+      <el-form-item>
+        <el-button
+          :disabled="loading"
+          @click="() => {}">
+          <cs-icon name="plus"/>
+          新增主属性
+        </el-button>
+      </el-form-item>
 
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            <cs-icon name="ban"/>
+            不检索
+          </el-button>
+
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            <cs-icon name="bookmark-o"/>
+            关键字
+          </el-button>
+
+          <el-button
+            :disabled="loading"
+            @click="() => {}">
+            <cs-icon name="circle-o-notch"/>
+            范围
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button
+          :disabled="loading"
+          @click="() => {}">
+          <cs-icon name="trash-o"/>
+          删除
+        </el-button>
+      </el-form-item>
+
+      <cs-help
+        :router="$route.path"
+        style="padding-bottom: 19px;">
+      </cs-help>
+    </el-form>
+
+    <el-table
+      v-loading="loading"
+      :data="currentTableData"
+      row-key="goods_attribute_id"
+      :tree-props="{children: 'get_attribute'}"
+      @selection-change="handleSelectionChange"
+      @sort-change="sortChange"
+      stripe>
+
+      <el-table-column type="selection" width="55"/>
+
+      <el-table-column
+        label="编号"
+        prop="goods_attribute_id"
+        sortable="custom"
+        width="160">
+      </el-table-column>
+
+      <el-table-column
+        label="名称"
+        prop="attr_name"
+        sortable="custom">
+      </el-table-column>
+
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    },
+    typeData: {
+      default: () => []
+    },
+    tableData: {
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      currentTableData: [],
+      multipleSelection: [],
+      auth: {
+      }
+    }
+  },
+  watch: {
+    tableData: {
+      handler(val) {
+        this.currentTableData = val
+      },
+      immediate: true
+    }
+  },
+  mounted() {
+    this._validationAuth()
+  },
+  methods: {
+    // 验证权限
+    _validationAuth() {
+    },
+    // 获取列表中的编号
+    _getIdList(val) {
+      if (val === null) {
+        val = this.multipleSelection
+      }
+
+      let idList = []
+      if (Array.isArray(val)) {
+        val.forEach(value => {
+          idList.push(value.goods_attribute_id)
+        })
+      } else {
+        idList.push(this.currentTableData[val].goods_attribute_id)
+      }
+
+      return idList
+    },
+    // 获取排序字段
+    sortChange({ column, prop, order }) {
+      let sort = {
+        order_type: undefined,
+        order_field: undefined
+      }
+
+      if (column && order) {
+        sort.order_type = order === 'ascending' ? 'asc' : 'desc'
+        sort.order_field = prop
+      }
+
+      this.$emit('sort', sort)
+    },
+    // 选中数据项
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
