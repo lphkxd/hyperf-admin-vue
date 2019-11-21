a1073f1d05b58e0449d52570ed612ea0b9448c87
思维空间 <dnyz520@126.com>
2019-11-22 01:03:07 +0800

    范例

--- src/views/system/aided/friendlink/components/PageMain/index.vue
+++ src/views/system/aided/friendlink/components/PageMain/index.vue
@@ -207,15 +207,22 @@
                 <cs-icon slot="reference" name="image"/>
               </el-popover>
             </template>
-            <cs-upload
+
+            <el-dropdown
               slot="append"
-              type="slot"
-              accept="image/*"
-              :limit="1"
-              :multiple="false"
-              @confirm="_getUploadFileList">
-              <el-button slot="control"><cs-icon name="upload"/></el-button>
-            </cs-upload>
+              :show-timeout="50"
+              @command="handleCommand">
+              <el-button><cs-icon name="cloud-upload" style="color: #909399;"/></el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="storage">
+                  <cs-icon name="inbox" class="cs-mr-5"/>资源选择
+                </el-dropdown-item>
+
+                <el-dropdown-item command="upload">
+                  <cs-icon name="upload" class="cs-mr-5"/>上传资源
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
           </el-input>
         </el-form-item>
 
@@ -268,6 +275,22 @@
           @click="update"
           size="small">修改</el-button>
       </div>
+
+      <cs-storage
+        ref="storage"
+        style="display: none"
+        @confirm="_getStorageFileList">
+      </cs-storage>
+
+      <cs-upload
+        style="display: none"
+        ref="upload"
+        type="slot"
+        accept="image/*"
+        :limit="1"
+        :multiple="false"
+        @confirm="_getUploadFileList">
+      </cs-upload>
     </el-dialog>
   </div>
 </template>
@@ -284,7 +307,8 @@ import util from '@/utils/util'
 
 export default {
   components: {
-    'csUpload': () => import('@/components/cs-upload')
+    'csUpload': () => import('@/components/cs-upload'),
+    'csStorage': () => import('@/components/cs-storage')
   },
   props: {
     loading: {
@@ -423,6 +447,19 @@ export default {
 
       return idList
     },
+    // 资源下拉框事件
+    handleCommand(command) {
+      switch (command) {
+        case 'storage':
+          this.$refs.storage.storageType = [0, 2]
+          this.$refs.storage.handleStorageDlg()
+          break
+
+        case 'upload':
+          this.$refs.upload.handleUploadDlg()
+          break
+      }
+    },
     // 获取上传文件
     _getUploadFileList(files) {
       if (!files.length) {
@@ -434,7 +471,22 @@ export default {
         return
       }
 
-      this.form.logo = response.data[0].url
+      if (response.data[0].type === 0) {
+        this.form.logo = response.data[0].url
+      }
+    },
+    // 获取选择文件
+    _getStorageFileList(files) {
+      if (!files.length) {
+        return
+      }
+
+      for (const value of files) {
+        if (value.type === 0) {
+          this.form.logo = value.url
+          break
+        }
+      }
     },
     // 选中数据项
     handleSelectionChange(val) {
